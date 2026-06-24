cfc027fbb3c5aed5c8f9dad0da1f663fe6c7538b
vu1Art1st <vu1art1st@users.noreply.github.com>
2026-06-24 11:09:22 +0800

    feat(utils): add fixed-length string split utility

---
+++ magnetos/utils/string_utils.py
@@ -0,0 +1,16 @@
+# Created by restran on 2017/9/26
+
+
+def fixed_length_split(data, width):
+    """
+    固定长度分割字符串
+    :param data:
+    :param width:
+    :return:
+    """
+    # 使用正则的方法
+    # import re
+    # split = re.findall(r'.{%s}' % width, string)
+    return [data[x: x + width] for x in range(0, len(data), width)]
+
+
