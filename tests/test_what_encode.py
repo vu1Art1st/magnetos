33f73ee2d304b758bbacb0518381a0e0c038f370
vu1Art1st <vu1art1st@users.noreply.github.com>
2026-06-24 11:09:46 +0800

    test: add WhatEncode unit test scaffolding

---
+++ tests/test_what_encode.py
@@ -0,0 +1,15 @@
+# Created by restran on 2017/9/21
+
+import logging
+import unittest
+
+logger = logging.getLogger(__name__)
+
+
+class WhatEncodeTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
