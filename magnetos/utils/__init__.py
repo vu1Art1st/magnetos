77392076a9644ed3a5df08d29baa49bc78523a2c
vu1Art1st <vu1art1st@users.noreply.github.com>
2026-06-24 11:09:16 +0800

    feat(utils): add shell command runner and raw plaintext restoration helper

---
+++ magnetos/utils/__init__.py
@@ -0,0 +1,44 @@
+# Created by restran on 2017/9/15
+import string
+import subprocess
+
+
+def run_shell_cmd(cmd):
+    try:
+        (stdout, stderr) = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                                            stderr=subprocess.PIPE, shell=True).communicate()
+        if stdout is None:
+            stdout = ''
+        if stderr is None:
+            stderr = ''
+        return '%s%s' % (stdout, stderr)
+
+    except Exception as e:
+        print(e)
+        print('!!!error!!!')
+        return ''
+
+
+def get_raw_plain_text(raw_data, decoded_data):
+    """
+    因为密文中可能包含数字，符合等各种非字母的字符，一些解密的算法是不考虑这些
+    在输出明文的时候，要跟这些符合，按要原来的顺序还原回来
+    :param raw_data:
+    :param decoded_data:
+    :return:
+    """
+    index = 0
+    plain = []
+    for i, c in enumerate(raw_data):
+        if c in string.ascii_lowercase:
+            new_c = decoded_data[index].lower()
+            index += 1
+        elif c in string.ascii_uppercase:
+            new_c = decoded_data[index].upper()
+            index += 1
+        else:
+            new_c = c
+
+        plain.append(new_c)
+
+    return ''.join(plain)
