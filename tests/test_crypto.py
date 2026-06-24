8c48446faf7a4fcfc7a06b1561666068b3a849d7
vu1Art1st <vu1art1st@users.noreply.github.com>
2026-06-24 11:09:46 +0800

    test: add crypto unit test scaffolding with mobile_keyboard import

---
+++ tests/test_crypto.py
@@ -0,0 +1,16 @@
+# Created by restran on 2017/10/15
+import os
+import unittest
+import logging
+from magnetos.crypto import mobile_keyboard
+
+logger = logging.getLogger(__name__)
+
+
+class CryptoTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
