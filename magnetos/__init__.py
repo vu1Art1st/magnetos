933570e9c091dd27e1ceb85ec74728daf5bcaa4e
vu1Art1st <vu1art1st@users.noreply.github.com>
2026-06-24 11:09:16 +0800

    chore: add package __init__ with version 0.7.0 and author metadata

---
+++ magnetos/__init__.py
@@ -0,0 +1,8 @@
+# Created by restran on 2017/8/29
+
+from . import utils as util
+
+__all__ = ['util', '__author__', '__version__']
+
+__author__ = "restran <grestran@gmail.com>"
+__version__ = '0.7.0'
