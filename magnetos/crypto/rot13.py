c10c908d9ded434d979ca6addbf6f9ef1a5323a8
vu1Art1st <vu1art1st@users.noreply.github.com>
2026-06-24 11:09:34 +0800

    feat(crypto): add ROT13 decoder — 13-shift Caesar variant for Latin alphabet

---
+++ magnetos/crypto/rot13.py
@@ -0,0 +1,35 @@
+# Created by restran on 2016/12/4
+import string
+from mountains.utils import PrintCollector
+
+# https://zh.wikipedia.org/wiki/ROT13
+
+"""
+ROT13 是过去在古罗马开发的凯撒加密的一种变体
+"""
+
+
+def decode_rot13(data):
+    buff = []
+    for i in data:
+        if i not in string.ascii_letters:
+            buff.append(i)
+        elif ord('A') <= ord(i) <= ord('Z'):
+            buff.append(chr((ord(i) - 13 + 26 - ord('A')) % 26 + ord('A')))
+        else:
+            buff.append(chr((ord(i) - 13 + 26 - ord('a')) % 26 + ord('a')))
+
+    return ''.join(buff)
+
+
+def decode(data):
+    p = PrintCollector()
+    d = decode_rot13(data)
+    p.print(d)
+    return p.all_output()
+
+
+if __name__ == '__main__':
+    # input_data = "synt{mur_VF_syn9_svtug1at}"
+    input_data = 'nffreg'
+    decode(input_data)
